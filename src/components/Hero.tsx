039b632a6c60345524cc97f85a0d696e5a63e772
biprotosh <biprotosh123@gmail.com>
2025-08-18 17:08:57 +0530

    Implement hero scroll animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         const heroSplit = new SplitText(".title", { type: "chars, words" });
         const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -49,6 +55,31 @@ const Hero = () => {
         })
             .to(".right-leaf", { y: 200 }, 0)
             .to(".left-leaf", { y: -200 }, 0);
+
+        /*
+            'top 50%' - the first property is the element we are animeting and the 2nd property is the screen
+            means, when the top of ther video reches the 50% down the screen the animation will happen
+        */
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "120% top" : "bottom top";
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        if (videoRef.current) {
+            videoRef.current.onloadedmetadata = () => {
+                tl.to(videoRef.current, {
+                    currentTime: videoRef.current?.duration,
+                })
+            };
+        }
     }, []);
 
     return (
@@ -85,6 +116,15 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     );
 };
